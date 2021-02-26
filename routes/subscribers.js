2316373e004358f0753990ba2f41b51a1fc356de
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-26 10:02:45 +0100

    reuseable function to get subscriber by id

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -11,3 +11,23 @@ router.get('/', async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
+
+
+
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(404).json({ message: 'Cannot find subscriber' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.subscriber = subscriber;
+  next();
+};
+
+module.exports = router;
