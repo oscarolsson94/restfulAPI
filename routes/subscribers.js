c50ce5f06f277436a4a5f2df4cce479e692a15fd
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-02-26 10:03:45 +0100

    updating and deleting by ID

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -31,6 +31,31 @@ router.post('/', async (req, res) => {
     }
 });
 
+// Updating One
+router.patch('/:id', getSubscriber, async (req, res) => {
+    if (req.body.name != null) {
+        res.subscriber.name = req.body.name;
+    }
+    if (req.body.subscribedToChannel != null) {
+        res.subscriber.subscribedToChannel = req.body.subscribedToChannel;
+    }
+    try {
+        const updatedSubscriber = await res.subscriber.save();
+        res.json(updatedSubscriber);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Deleting One
+router.delete('/:id', getSubscriber, async (req, res) => {
+    try {
+        await res.subscriber.remove();
+        res.json({ message: 'Deleted Subscriber' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
 
 async function getSubscriber(req, res, next) {
   let subscriber;
